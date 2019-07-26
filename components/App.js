85139cdc0ef3216233bc8011187f2783e80ae22c
Taggart Bowen-Gaddy <taggartbg@gmail.com>
2019-07-26 13:56:35 -0400

    Add initial packages
    - Add initial packages for building styles and running React

---
+++ components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        Hello, tBTC
+      </div>
+    )
+  }
+}
+
+export default App
