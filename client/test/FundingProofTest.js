cbe5d6cabca63acc6f8be1690f8f2f1975325936
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-07-29 11:12:59 +0200

    Updates to funding proof script
    - extract config file to a variable instead of requiring it as a
    parameter
    - add docs

--- client/test/FundingProofTest.js
+++ client/test/FundingProofTest.js
@@ -1,5 +1,5 @@
-const FundingProof = require('../src/FundingProof')
-const ElectrumClient = require('tbtc-helpers').ElectrumClient
+const rewire = require('rewire')
+const FundingProof = rewire('../src/FundingProof')
 
 const fs = require('fs')
 const chai = require('chai')
@@ -8,26 +8,12 @@ const assert = chai.assert
 const TX_ID = '72e7fd57c2adb1ed2305c4247486ff79aec363296f02ec65be141904f80d214e'
 const CONFIRMATIONS = 6
 
-
 describe('FundingProof', async () => {
-  let electrumConfig
-
-  before(async () => {
-    const configFile = fs.readFileSync(process.env.CONFIG_FILE, 'utf8')
-    config = JSON.parse(configFile)
-
-    electrumConfig = new ElectrumClient.Config(
-      config.electrum.testnet.server,
-      config.electrum.testnet.port,
-      config.electrum.testnet.protocol
-    )
-  })
-
-  it('getProof', async () => {
+  it('getTransactionProof', async () => {
     const proofFile = fs.readFileSync('./test/data/proof.json', 'utf8')
     const expectedResult = JSON.parse(proofFile)
 
-    const result = await FundingProof.getTransactionProof(electrumConfig, TX_ID, CONFIRMATIONS)
+    const result = await FundingProof.__get__('getTransactionProof')(TX_ID, CONFIRMATIONS)
 
     assert.deepEqual(result, expectedResult)
   })
