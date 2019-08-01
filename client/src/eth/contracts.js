f79d957aa16bfc56f930a040757fa2641f784c82
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-01 20:05:35 +0200

    Helper to load contract artifacts with defaults

---
+++ client/src/eth/contracts.js
@@ -0,0 +1,39 @@
+const TruffleContract = require("truffle-contract");
+
+// Just a simple wrapper so we can prototype quickly
+const artifacts = {
+  require: (name) => {
+    if(name.startsWith('./')) throw new Error("bad path")
+    let contractName = name.split('.sol')[0]
+    const json = require(`./artifacts/${contractName}.json`)
+    const contract = TruffleContract(json)
+    
+    return contract
+  }
+}
+
+export const Deposit = artifacts.require('Deposit.sol')
+export const TBTCSystem = artifacts.require('TBTCSystem.sol')
+export const TBTCToken = artifacts.require('TBTCToken.sol')
+export const KeepBridge = artifacts.require('KeepBridge.sol')
+export const DepositFactory = artifacts.require('DepositFactory.sol')
+
+const contracts = [
+  Deposit,
+  TBTCSystem,
+  TBTCToken,
+  KeepBridge,
+  DepositFactory
+]
+
+export async function setDefaults(web3) {
+  const accounts = await web3.eth.getAccounts()
+  const from = accounts[0]
+
+  const txDefaults = { from }
+
+  for(let contract of contracts) {
+    contract.setProvider(web3.currentProvider)
+    contract.defaults(txDefaults)
+  }
+}
