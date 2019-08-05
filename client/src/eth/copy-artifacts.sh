cfbd1c7e4d4ca17a0a8593c3b1bc873904c20d90
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-05 16:41:15 +0200

    Document temp script

--- client/src/eth/copy-artifacts.sh
+++ client/src/eth/copy-artifacts.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# 
+# This is a temporary tool to copy the most recent artifacts of tBTC
+# into this package.
+# 
+# TODO: just for development, until we get artifacts uploaded somewhere properly.
 set -ex
 
 for CONTRACT in Deposit TBTCSystem TBTCToken KeepBridge DepositFactory; do
