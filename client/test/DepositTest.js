df592b204959b80c190d72d7cb84f1a1a4aa03d3
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-05 16:38:52 +0200

    Doc Web3 hack

--- client/test/DepositTest.js
+++ client/test/DepositTest.js
@@ -18,8 +18,13 @@ chai.use(bnChai(BN))
 let web3
 
 describe("Ethereum helpers", async () => {
-    before(async () => {        
+    before(async () => {
+        // TruffleContract was built to use web3 0.3.0, which uses an API method of `sendAsync`
+        // in later versions of web (1.0.0), this method was renamed to `send`
+        // This hack makes them work together again.
+        // https://github.com/ethereum/web3.js/issues/1119
         Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send
+        
         web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
         await setDefaults(web3)
     })
