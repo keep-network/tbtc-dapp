4e327d0919a22bd549e793af7f363cbb87c0d543
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-06 18:58:56 +0200

    Consistent export syntax

--- client/src/FundingProof.js
+++ client/src/FundingProof.js
@@ -1,5 +1,4 @@
-const BitcoinTxParser = require('tbtc-helpers').BitcoinTxParser
-const bitcoinspv = require('tbtc-helpers').BitcoinSPV
+import { BitcoinTxParser, BitcoinSPV as bitcoinspv } from 'tbtc-helpers'
 
 /**
  * Gets transaction SPV proof from BitcoinSPV.
@@ -32,7 +31,7 @@ async function getTransactionProof(electrumClient, txID, confirmations) {
  * @param {string} txID Funding transaction ID.
  * @param {number} fundingOutputIndex Position of a funding output in the transaction.
  */
-async function calculateAndSubmitFundingProof(electrumClient, txID, fundingOutputIndex) {
+export async function calculateAndSubmitFundingProof(electrumClient, txID, fundingOutputIndex) {
   if (txID.length != 64) {
     throw new Error(`invalid transaction id length [${txID.length}], required: [64]`)
   }
@@ -61,8 +60,4 @@ async function calculateAndSubmitFundingProof(electrumClient, txID, fundingOutpu
 
 
   // return eth transaction id to later convert it to etherscan link
-}
-
-module.exports = {
-  calculateAndSubmitFundingProof,
 }
