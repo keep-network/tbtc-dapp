97b9071fa2ddad6c296c86d1379df444d873cabe
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-13 14:50:36 +0200

    Add ECDSAKeep via script

--- client/src/eth/copy-artifacts.sh
+++ client/src/eth/copy-artifacts.sh
@@ -10,5 +10,9 @@ for CONTRACT in Deposit TBTCSystem TBTCToken KeepBridge DepositFactory; do
     cp ../../../../tbtc/implementation/build/contracts/$CONTRACT.json artifacts/
 done
 
+for CONTRACT in ECDSAKeep; do
+    cp ../../../../keep-tecdsa/solidity/build/contracts/$CONTRACT.json artifacts/
+done
+
 # Can easily replace later with:
 # wget s3.amazonaws.com/keep/testnet/ABC.json
