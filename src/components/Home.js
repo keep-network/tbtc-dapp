5cdc37aaee8e7b2f81cfcb8746cfb0c6adef5338
Taggart Bowen-Gaddy <taggart.bowen.gaddy@foldapp.com>
2019-08-14 01:04:37 -0400

    Typography styling in place

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,35 +9,30 @@ const Home = ({ history }) => (
       <br />
       Ethereum
       <div className="subtitle">
-        No KYC, no middlemen, no bullshit.
+        <div className="vertical-aligned">
+          No KYC, no middlemen, no bullshit.
+        </div>
       </div>
     </div>
-    <ol className="step-by-step">
-      <li>
-        <div className="number">
-          01
-        </div>
-        <div className="instruction">
-          Deposit BTC
-        </div>
-      </li>
-      <li>
-        <div className="number">
-          02
-        </div>
-        <div className="instruction">
-          Mint tBTC
-        </div>
-      </li>
-      <li>
-        <div className="number">
-          03
-        </div>
-        <div className="instruction">
-          Lend and earn interest on your BTC.
-        </div>
-      </li>
-    </ol>
+    <div className="step-by-step-container">
+      <ol className="step-by-step">
+        <li>
+          <div className="instruction">
+            Deposit BTC
+          </div>
+        </li>
+        <li>
+          <div className="instruction">
+            Mint tBTC
+          </div>
+        </li>
+        <li>
+          <div className="instruction">
+            Lend and earn interest on your BTC.
+          </div>
+        </li>
+      </ol>
+    </div>
     <div className="mission-statement">
       <div className="hook">
         No middlemen. Period.
