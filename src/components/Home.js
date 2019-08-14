e81372d3bd3c5cbe234342b20068f5ea5057b181
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-08-14 11:12:45 +0200

    Change Home to Component and add link to Start
    Changed Home page to a component and added a link to transitoin to Start
    page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = ({ history }) => (
-  <div className="home">
-    <div className="title">
-      Bitcoin,
-      <br />
-      On
-      <br />
-      Ethereum
-      <div className="subtitle">
-        <div className="vertical-aligned">
-          No KYC, no middlemen, no bullshit.
+import history from '../history'
+
+class Home extends Component {
+
+  handleClickPay = (evt) => {
+    evt.preventDefault()
+    evt.stopPropagation()
+
+    history.push('/start')
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="title">
+          Bitcoin,
+		      <br />
+          On
+		      <br />
+          Ethereum
+		      <div className="subtitle">
+            <div className="vertical-aligned">
+              No KYC, no middlemen, no bullshit.
+		        </div>
+          </div>
+        </div>
+        <div className="step-by-step">
+          <ol>
+            <li>
+              <a href="/start" onClick={this.handleClickPay}>
+                Deposit BTC
+              </a>
+            </li>
+            <li>
+              Mint tBTC
+		        </li>
+            <li>
+              Lend and earn interest on your BTC.
+		        </li>
+          </ol>
+        </div>
+        <div className="mission-statement">
+          <div className="hook">
+            No middlemen. Period.
+		      </div>
+          <div className="line-and-sinker">
+            Censorship resistant, seizure resistant, inflation resistant
+		      </div>
         </div>
       </div>
-    </div>
-    <div className="step-by-step">
-      <ol>
-        <li>
-          Deposit BTC
-        </li>
-        <li>
-          Mint tBTC
-        </li>
-        <li>
-          Lend and earn interest on your BTC.
-        </li>
-      </ol>
-    </div>
-    <div className="mission-statement">
-      <div className="hook">
-        No middlemen. Period.
-      </div>
-      <div className="line-and-sinker">
-        Censorship resistant, seizure resistant, inflation resistant
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Home
