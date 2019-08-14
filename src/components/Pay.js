ad0c5fdf60c14c2cc62edeaa304fc84640047621
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-14 16:15:25 +0200

    Change Pay copy

--- src/components/Pay.js
+++ src/components/Pay.js
@@ -33,7 +33,7 @@ class Pay extends Component {
       renderTop = (
         <div className="qr-code">
           <QRCode
-            value={address}
+            value={address || '23980q9wufeu0q9832fy40cnw9qr3092n8q09ndq902n8jq390jnaewjdhfaoiweuhfoawiufhaowifuhaoiewuhfaowieufhaowieufhaoiseufhaowieuhfaoiweuhfaowieuhofiwesuahfoeiauhwefiauweh'}
             renderAs="svg"
             size={225} />
         </div>
@@ -75,7 +75,7 @@ class Pay extends Component {
           <hr />
           <div className="description">
             <div>
-              Scan the QR code or tap to pay, or copy the address below into your wallet
+              Scan the QR code or click to copy the address below into your wallet
             </div>
             <div className="custodial-fee">
               <span className="custodial-fee-label">Custodial Fee: </span>
