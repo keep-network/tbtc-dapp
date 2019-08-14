54328e684a0c8da3edb4483e696037a69a8818ca
Taggart Bowen-Gaddy <taggart.bowen.gaddy@foldapp.com>
2019-08-14 00:31:59 -0400

    Home HTML in place

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,50 @@ import React from 'react';
 
 const Home = ({ history }) => (
   <div className="home">
-    <button onClick={() => {history.push('/start')}}>Make a Deposit</button>
+    <div className="title">
+      Bitcoin,
+      <br />
+      On
+      <br />
+      Ethereum
+      <div className="subtitle">
+        No KYC, no middlemen, no bullshit.
+      </div>
+    </div>
+    <ol className="step-by-step">
+      <li>
+        <div className="number">
+          01
+        </div>
+        <div className="instruction">
+          Deposit BTC
+        </div>
+      </li>
+      <li>
+        <div className="number">
+          02
+        </div>
+        <div className="instruction">
+          Mint tBTC
+        </div>
+      </li>
+      <li>
+        <div className="number">
+          03
+        </div>
+        <div className="instruction">
+          Lend and earn interest on your BTC.
+        </div>
+      </li>
+    </ol>
+    <div className="mission-statement">
+      <div className="hook">
+        No middlemen. Period.
+      </div>
+      <div className="line-and-sinker">
+        Censorship resistant, seizure resistant, inflation resistant
+      </div>
+    </div>
   </div>
 )
 
