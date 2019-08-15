8d938ab0321c76f166d90826f09dfab96c156720
Taggart Bowen-Gaddy <taggart.bowen.gaddy@foldapp.com>
2019-08-15 11:16:54 -0400

    Success and error states for adding emails

--- src/components/lib/Footer.js
+++ src/components/lib/Footer.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
 import TBTCLogo from '../svgs/TBTCLogo'
+import Check from '../svgs/Check'
 
 const validEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-class Header extends Component {
+class Footer extends Component {
   state = {
-    email: ''
+    email: '',
+    success: false,
+    error: ''
   }
 
   handleInput = (evt) => {
@@ -20,15 +23,28 @@ class Header extends Component {
     const { email } = this.state
 
     if (email.match(validEmailRegex)) {
-      console.log("SUCCESS: ", email)
+      fetch('/mailing-list/signup', {
+        method: 'POST',
+        data: JSON.stringify({ email })
+      }).then(res => res.json())
+        .then(data => {
+          console.log("SUCCESS: ", data)
+          this.setState({ success: true, error: '' })
+        })
+        .catch(err => {
+          console.log("ERROR: ", err)
+          this.setState({ error: err.toString() })
+        })
     } else {
-      console.log("Failure: ", email)
+      this.setState({
+        error: 'Invalid email'
+      })
     }
   }
 
   render() {
     const { includeSubscription } = this.props
-    const { email } = this.state
+    const { email, error, success } = this.state
 
     return (
       <footer className={includeSubscription ? 'include-subscription' : ''}>
@@ -56,6 +72,18 @@ class Header extends Component {
                   placeholder="enter your email to recieve updates" />
                 <input type="submit" value="Submit >>>>" />
               </form>
+              { error
+                ? <div className="error">
+                    { error }
+                  </div>
+                : ''
+              }
+              { success
+                ? <div className="success">
+                    <Check width="80px" height="80px" />
+                  </div>
+                : ''
+              }
             </div>
           )
         }
@@ -76,4 +104,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default Footer
