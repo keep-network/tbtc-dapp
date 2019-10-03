ea939d026f38dd217b84b598938becb34da5d89e
Taggart Bowen-Gaddy <taggart.bowen.gaddy@thesis.co>
2019-10-03 16:04:16 -0400

    Button styling in place

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,44 @@ import history from '../history'
 
 class Home extends Component {
 
-  handleClickPay = (evt) => {
+  state = {
+    hoverDeposit: false,
+    hoverRedeem: false
+  }
+
+  handleClickDeposit = (evt) => {
     evt.preventDefault()
     evt.stopPropagation()
 
-    history.push('/start')
+    history.push('/deposit/start')
+  }
+
+  handleClickRedeem = (evt) => {
+    evt.preventDefault()
+    evt.stopPropagation()
+
+    history.push('/redemption/initialize')
+  }
+
+  handleHoverDeposit = () => {
+    this.setState({ hoverDeposit: true })
+  }
+
+  handleHoverLeaveDeposit = () => {
+    this.setState({ hoverDeposit: false })
+  }
+
+  handleHoverRedeem = () => {
+    this.setState({ hoverRedeem: true })
+  }
+
+  handleHoverLeaveRedeem = () => {
+    this.setState({ hoverRedeem: false })
   }
 
   render() {
+    const { hoverDeposit, hoverRedeem } = this.state
+
     return (
       <div className="home">
         <div className="title">
@@ -26,12 +56,22 @@ class Home extends Component {
 		        </div>
           </div>
         </div>
+        <div className="mint-or-redeem">
+          <a href="/deposit/start" onClick={this.handleClickDeposit}>
+            <div className="button blue" onMouseOver={this.handleHoverDeposit} onMouseLeave={this.handleHoverLeaveDeposit}>
+              Deposit {hoverDeposit ? '>>>' : ''}
+            </div>
+          </a>
+          <a href="/redemption/initialize" onClick={this.handleClickRedeem}>
+            <div className="button black" onMouseOver={this.handleHoverRedeem} onMouseLeave={this.handleHoverLeaveRedeem}>
+              Redeem {hoverRedeem ? '>>>' : ''}
+            </div>
+          </a>
+        </div>
         <div className="step-by-step">
           <ol>
             <li>
-              <a href="/start" onClick={this.handleClickPay}>
-                Deposit BTC
-              </a>
+              Deposit BTC
             </li>
             <li>
               Mint tBTC
