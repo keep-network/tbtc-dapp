72645a54df461663fad8087190cf5621913894cd
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-11-11 11:51:21 +0100

    Function to get keep public key
    Added a function to get a public key of a keep registered in deposit contract.
    Concatenates X and Y coordinates and returns a value as a buffer.

--- client/src/Deposit.js
+++ client/src/Deposit.js
@@ -7,6 +7,8 @@ import {
   truffleToWeb3Contract,
 } from './eth/contracts'
 
+const web3 = require('web3')
+
 /**
  * Creates a new deposit and returns its address
  * @return {string} Address of the Deposit contract instance
@@ -97,4 +99,29 @@ export async function watchForPublicKeyPublished(depositAddress) {
       3000 // every 3 seconds
     )
   })
+}
+
+/**
+ * Gets public key of the keep. Finds a public key registration event and
+ * returns a public key emitted in the event. Public key is logged in the event
+ * as x and y coordinates, it concatenates these values.
+ * @param {string} depositAddress Address of the deposit.
+ * @return {Buffer} Public key as concatenated x and y coordinates.
+ */
+export async function getKeepPublicKey(depositAddress) {
+  const depositLog = await TBTCSystem.deployed()
+
+  const publickKeyEvents = await depositLog.getPastEvents(
+    'RegisteredPubkey',
+    {
+      fromBlock: '0',
+      toBlock: 'latest',
+      filter: { _depositContractAddress: depositAddress },
+    }
+  )
+
+  const publicKeyX = Buffer.from(web3.utils.hexToBytes(publickKeyEvents[0].returnValues._signingGroupPubkeyX))
+  const publicKeyY = Buffer.from(web3.utils.hexToBytes(publickKeyEvents[0].returnValues._signingGroupPubkeyY))
+
+  return Buffer.concat([Buffer.from(publicKeyX), Buffer.from(publicKeyY)])
 }
