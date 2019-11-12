d173f827ae1d2ecfebd1ae89433589f4f9d0e02f
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-11-12 13:33:07 +0100

    Implement redemption sagas
    Implemented calls to scripts required to execute redemption flow with
    tbtc client.

--- src/sagas/redemption.js
+++ src/sagas/redemption.js
@@ -3,6 +3,19 @@ import { call, put, select, delay } from 'redux-saga/effects'
 
 import { navigateTo } from '../lib/router/actions'
 
+import {
+    requestRedemption,
+    createUnsignedTransaction,
+    watchForSignatureSubmitted,
+    provideRedemptionSignature,
+    combineSignedTransaction,
+    broadcastBTCTransaction,
+    provideRedemptionProof,
+    waitForConfirmations
+} from 'tbtc-client'
+
+import { getElectrumClient } from './deposit'
+
 export const UPDATE_ADDRESSES = 'UPDATE_ADDRESSES'
 export const UPDATE_TRANSACTION_AND_SIGNATURE = 'UPDATE_TRANSACTION_AND_SIGNATURE'
 export const UPDATE_TX_HASH = 'UPDATE_TX_HASH'
@@ -12,7 +25,13 @@ export const REDEMPTION_PROVE_BTC_TX_BEGIN = 'REDEMPTION_PROVE_BTC_TX_BEGIN'
 export const REDEMPTION_PROVE_BTC_TX_SUCCESS = 'REDEMPTION_PROVE_BTC_TX_SUCCESS'
 export const REDEMPTION_PROVE_BTC_TX_ERROR = 'REDEMPTION_PROVE_BTC_TX_ERROR'
 
+// TODO: rename to `requestRedemption`?
 export function* saveAddresses({ payload }) {
+    const { depositAddress, btcAddress } = payload
+
+    console.log(`start redemption of deposit [${depositAddress}] to bitcoin address [${btcAddress}]`)
+    yield call(requestRedemption, depositAddress, btcAddress)
+
     yield put({
         type: UPDATE_ADDRESSES,
         payload
@@ -22,31 +41,38 @@ export function* saveAddresses({ payload }) {
 }
 
 export function* buildTransactionAndSubmitSignature() {
-    const btcAddress = yield select(state => state.redemption.btcAddress)
-    // TODO: Build Transaction
-    const transaction = 'TODO'
+    const depositAddress = yield select(state => state.redemption.depositAddress)
 
-    // TODO: Wait for + Save Signature
-    const signature = 'TODO'
+    console.log(`build redemption transaction`)
+    const unsignedTransaction = yield call(createUnsignedTransaction, depositAddress)
+
+    console.log(`watch for transaction signature`)
+    const signature = yield call(watchForSignatureSubmitted, depositAddress, unsignedTransaction.digest)
+
+    console.log(`submit signature to deposit contract`)
+    yield call(provideRedemptionSignature, depositAddress, signature.r, signature.s, signature.recoveryID)
 
     yield put({
         type: UPDATE_TRANSACTION_AND_SIGNATURE,
-        payload: { transaction, signature }
+        payload: { unsignedTransaction, signature }
     })
-
-    const depositAddress = yield select(state => state.redemption.depositAddress)
-    // TODO: Submit Signature
 
     yield put(navigateTo('/redeem/confirming'))
 }
 
 export function* broadcastTransaction() {
-    const transaction = yield select(state => state.redemption.transaction)
+    const depositAddress = yield select(state => state.redemption.depositAddress)
+    const unsignedTransaction = yield select(state => state.redemption.unsignedTransaction)
     const signature = yield select(state => state.redemption.signature)
-    // TODO: Broadcast Signed Transaction
 
-    // TODO: Save txHash to state
-    const txHash = 'TODO'
+    console.log(`add signature to redemption transaction`)
+    const signedTransaction = yield call(combineSignedTransaction, depositAddress, unsignedTransaction.hex, signature.r, signature.s)
+
+    console.log(`broadcast redemption transaction`)
+    const electrumClient = yield call(getElectrumClient)
+    const txHash = yield call(broadcastBTCTransaction, electrumClient, signedTransaction)
+
+    electrumClient.close()
 
     yield put({
         type: UPDATE_TX_HASH,
@@ -58,24 +84,22 @@ export function* broadcastTransaction() {
 
 export function* pollForConfirmations() {
     let confirmations = 0
+    const txHash = yield select(state => state.redemption.txHash)
     const requiredConfirmations = yield select(state => state.redemption.requiredConfirmations)
 
-    while(confirmations < requiredConfirmations) {
-        try {
-            // TODO: yield call to electrum, request confirmations
+    const electrumClient = yield call(getElectrumClient)
 
-            // TODO: Update newConfirmations with real data
-            const newConfirmations = confirmations + 1
+    console.log(`wait for [${requiredConfirmations}] transaction confirmations`)
+    while (confirmations < requiredConfirmations) {
+        try {
+            confirmations = yield call(waitForConfirmations, electrumClient, txHash, confirmations)
+            console.log(`got [${confirmations}] transaction confirmations`)
 
             yield put({
                 type: UPDATE_CONFIRMATIONS,
-                payload: { confirmations: newConfirmations }
+                payload: { confirmations }
             })
-
-            yield delay(10000)
-
-            confirmations = yield select( state => state.redemption.confirmations)
-        } catch(err) {
+        } catch (err) {
             yield put({
                 type: POLL_FOR_CONFIRMATIONS_ERROR,
                 payload: { pollForConfirmationsError: err.toString() }
@@ -84,6 +108,8 @@ export function* pollForConfirmations() {
             break
         }
     }
+
+    electrumClient.close()
 
     const pollForConfirmationsError = yield select(state => state.redemption.pollForConfirmationsError)
     if (!pollForConfirmationsError) {
@@ -94,12 +120,17 @@ export function* pollForConfirmations() {
 export function* submitRedemptionProof() {
     yield put({ type: REDEMPTION_PROVE_BTC_TX_BEGIN })
 
-    // TODO: Do proof stuff
+    const depositAddress = yield select(state => state.redemption.depositAddress)
+    const txHash = yield select(state => state.redemption.txHash)
 
+    const electrumClient = yield call(getElectrumClient)
+
+    console.log(`submit redemption transaction proof`)
     try {
+        yield call(provideRedemptionProof, depositAddress, txHash, electrumClient)
+
         yield put({
             type: REDEMPTION_PROVE_BTC_TX_SUCCESS,
-            payload: {}
         })
 
         yield put(navigateTo('/redeem/congratulations'))
@@ -108,5 +139,7 @@ export function* submitRedemptionProof() {
             type: REDEMPTION_PROVE_BTC_TX_ERROR,
             payload: { error: err.toString() }
         })
+    } finally {
+        electrumClient.close()
     }
 }
