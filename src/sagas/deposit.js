79968c7e2dd25370b9c7fa450a7937306cef2d08
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-11-15 14:53:29 +0100

    Fix renamed funding tx property

--- src/sagas/deposit.js
+++ src/sagas/deposit.js
@@ -105,7 +105,7 @@ export function* waitConfirmation() {
         type: BTC_TX_MINED,
         payload: {
             btcDepositedTxID: fundingTx.transactionID,
-            fundingOutputIndex: fundingTx.fundingOutputPosition
+            fundingOutputIndex: fundingTx.outputPosition
         }
     })
 
