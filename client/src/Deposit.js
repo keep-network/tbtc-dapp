e9d840cad865da8bec76f9fa36fbb30155337a1c
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-11-15 10:31:33 +0100

    Moved get keep address and public key
    Moved getKeepAddress and getKeepPublicKey functions to a dedicated file
    as it's not specific to deposit flow but we will use it in other scripts
    too.

--- client/src/Deposit.js
+++ client/src/Deposit.js
@@ -7,7 +7,7 @@ import {
   truffleToWeb3Contract,
 } from './eth/contracts'
 
-const web3 = require('web3')
+import { getKeepAddress } from './eventslog'
 
 /**
  * Creates a new deposit and returns its address
@@ -45,22 +45,12 @@ export async function createDeposit() {
   return depositAddress
 }
 
-/**
- * Gets address of a keep created for the deposit. Finds the deposit creation
- * event and returns keep value emitted in the event.
- * @param {string} depositAddress Address of the deposit.
- * @return {string} Address of the keep.
- */
-export async function getKeepAddress(depositAddress) {
-  const tbtcSystem = await TBTCSystem.deployed()
-  const events = await tbtcSystem.getPastEvents('Created', {
-    fromBlock: 0,
-    toBlock: 'latest',
-    filter: { _depositContractAddress: depositAddress },
-  })
-  return events[0].returnValues._keepAddress
-}
 
+/**
+ * Waits for Public Key publication event emitted by keep.
+ * @param {string} depositAddress Address of the deposit.
+ * @return {Buffer} Public key as concatenated x and y coordinates.
+ */
 export async function watchForPublicKeyPublished(depositAddress) {
   return new Promise(async (resolve, reject) => {
     const keepAddress = await getKeepAddress(depositAddress)
@@ -101,27 +91,3 @@ export async function watchForPublicKeyPublished(depositAddress) {
   })
 }
 
-/**
- * Gets public key of the keep. Finds a public key registration event and
- * returns a public key emitted in the event. Public key is logged in the event
- * as x and y coordinates, it concatenates these values.
- * @param {string} depositAddress Address of the deposit.
- * @return {Buffer} Public key as concatenated x and y coordinates.
- */
-export async function getKeepPublicKey(depositAddress) {
-  const depositLog = await TBTCSystem.deployed()
-
-  const publickKeyEvents = await depositLog.getPastEvents(
-    'RegisteredPubkey',
-    {
-      fromBlock: '0',
-      toBlock: 'latest',
-      filter: { _depositContractAddress: depositAddress },
-    }
-  )
-
-  const publicKeyX = Buffer.from(web3.utils.hexToBytes(publickKeyEvents[0].returnValues._signingGroupPubkeyX))
-  const publicKeyY = Buffer.from(web3.utils.hexToBytes(publickKeyEvents[0].returnValues._signingGroupPubkeyY))
-
-  return Buffer.concat([Buffer.from(publicKeyX), Buffer.from(publicKeyY)])
-}
