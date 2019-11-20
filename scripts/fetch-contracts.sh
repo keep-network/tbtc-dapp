6fbce3c4552089fbded66ee4e94bcc92a6461522
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-11-20 17:46:12 +0100

    Script to fetch deployed contracts artifacts
    Added a script to download contracts artifacts published to the Google
    Cloud Bucket.
    The script contains paths to contracts required by the dApp.
    It expects `CONTRACT_DATA_BUCKET` environment variable to be set with
    the name of the bucket from which contracts should be downloaded.
    
    Example:
      CONTRACT_DATA_BUCKET=keep-dev-contract-data ./fetch-contracts.sh

---
+++ scripts/fetch-contracts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script fetches contracts artifacts published to the GCP bucket. It is expected
+# that `CONTRACT_DATA_BUCKET` environment variable is set to the name of a bucket
+# from which contracts should be downloaded.
+# 
+# Sample execution command:
+# CONTRACT_DATA_BUCKET=keep-dev-contract-data ./fetch-contracts.sh
+
+CONTRACTS_PATHS=(
+  "tbtc/Deposit.json" 
+  "tbtc/DepositFactory.json" 
+  "tbtc/TBTCConstants.json"
+  "tbtc/TBTCSystem.json"
+  "tbtc/TBTCToken.json"
+  "keep-tecdsa/ECDSAKeep.json"
+  )
+
+DESTINATION_DIR=$(realpath $(dirname $0)/../client/src/eth/artifacts)
+
+function create_destination_dir() {
+  mkdir -p $DESTINATION_DIR
+}
+
+function fetch_contracts() {
+  for CONTRACT_PATH in ${CONTRACTS_PATHS[@]}
+  do
+    gsutil -q cp gs://${CONTRACT_DATA_BUCKET}/${CONTRACT_PATH} $DESTINATION_DIR
+  done
+}
+
+echo "Fetch contracts artifacts to: ${DESTINATION_DIR}"
+create_destination_dir
+fetch_contracts
