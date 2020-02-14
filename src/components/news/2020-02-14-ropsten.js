5a6a6306bdf7d68f797e373bee9e629d3818f260
Antonio Salazar Cardozo <antonio@keep.network>
2020-02-13 19:18:24 -0500

    Fix some links and a date in Ropsten announcement

--- src/components/news/2020-02-14-ropsten.js
+++ src/components/news/2020-02-14-ropsten.js
@@ -4,14 +4,14 @@ import NewsItem from "./NewsItem"
 export default () => {
     let data = {
         title: "tBTC on Ropsten",
-        date: '02.12.2020',
+        date: '02.14.2020',
         body: `
         tBTC is Open-Source and Live on Ropsten
 
         The first release of tBTC is now live on Ropsten, the public Ethereum testnet.
 
         To try minting your first TBTC, visit the
-        <a href="htps://dapp.test.tbtc.network">reference dApp</a>. Make sure
+        <a href="http://dapp.test.tbtc.network">reference dApp</a>. Make sure
         you're loaded up on testnet bitcoin and Ropsten ether, and you can be
         the first TBTC whale :sunglasses_emoji:
 
@@ -23,11 +23,11 @@ export default () => {
 
         -> <a href="http://docs.keep.network/tbtc/index.pdf">Read the latest spec</a>
 
-        -> <a href="https://github.com/keep-network/tbtc/implementation">Browse the source code</a>
+        -> <a href="https://github.com/keep-network/tbtc/tree/master/implementation">Browse the source code</a>
 
-        -> <a href="https://dapp.test.tbtc.network/">Play with the dApp</a>
+        -> <a href="http://dapp.test.tbtc.network/">Play with the dApp</a>
 
-        -> <a href="https://www.npmjs.com/package/@keep-network/tbtc.js">Build on testnet (NPM)</a>
+        -> <a href="https://www.npmjs.com/package/@keep-network/tbtc.js">Build on testnet</a>
         `
     }
     return <NewsItem {...data}/>
