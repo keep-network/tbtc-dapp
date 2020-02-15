2ddc726dbb8e11b0b5afbdf3091ab4a9e9ad93c4
Matt Luongo <mhluongo@gmail.com>
2020-02-14 23:07:41 -0500

    Use the <code>

--- src/components/news/2020-02-14-announcing-tbtc-js.js
+++ src/components/news/2020-02-14-announcing-tbtc-js.js
@@ -6,11 +6,11 @@ export default () => {
         title: "Announcing tbtc.js",
         date: '02.14.2020',
       body: `
-        A new library, <a href="https://github.com/keep-network/tbtc.js"><pre>tbtc.js</pre></a>,
+        A new library, <a href="https://github.com/keep-network/tbtc.js"><code>tbtc.js</code></a>,
         is live on GitHub, representing the easiest, safest way to integrate
         Bitcoin in your Ethereum dApp.
 
-        <pre>tbtc.js</pre> makes it easy for developers to directly integrate
+        <code>tbtc.js</code> makes it easy for developers to directly integrate
         Bitcoin into their dApps. That means products like
         <a href="https://compound.finance/">Compound</a>,
         <a href="https://fulcrum.trade/">Fulcrum</a>, and
@@ -21,7 +21,7 @@ export default () => {
         interacting with the Bitcoin network through an Electrum
         client and Ethereum through a standard Web3 provider.
 
-        With <pre>tbtc.js</pre>, you can
+        With <code>tbtc.js</code>, you can
 
         -> <a href="https://github.com/keep-network/tbtc.js#creating-and-funding-a-deposit">Accept Bitcoin deposits in your dApp</a>
 
