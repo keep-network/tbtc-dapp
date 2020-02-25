7fe9b52f7dd0a61d95e3f3b89fe146d8e87caf4b
Antonio Salazar Cardozo <antonio@keep.network>
2020-02-24 21:18:04 -0500

    Display correct mint amount on deposit Congratulations page
    The amount is now calculated and displayed, rather than displaying a
    hard-coded 1 TBTC irrespective of lot size and signer fee.

--- src/components/deposit/Congratulations.js
+++ src/components/deposit/Congratulations.js
@@ -3,8 +3,15 @@ import { connect } from 'react-redux'
 
 import StatusIndicator from '../svgs/StatusIndicator'
 import TLogo from '../svgs/tlogo'
+import { BitcoinHelpers } from '@keep-network/tbtc.js'
 
-const Congratulations = ({ depositAddress }) => {
+import BigNumber from "bignumber.js"
+BigNumber.set({ DECIMAL_PLACES: 8 })
+
+const Congratulations = ({ depositAddress, lotInSatoshis, signerFeeInSatoshis }) => {
+  const mintedSatoshis = lotInSatoshis.sub(signerFeeInSatoshis)
+  const lotInTbtc = (new BigNumber(mintedSatoshis.toString())).div(BitcoinHelpers.satoshisPerBtc.toString())
+
   return <div className="congratulations">
     <div className="page-top">
       <StatusIndicator purple>
@@ -21,7 +28,7 @@ const Congratulations = ({ depositAddress }) => {
       <hr />
       <div className="description">
         <div className="description-content">
-          You are now the proud beneficiary of 1 TBTC
+          You are now the proud beneficiary of {lotInTbtc.toNumber()} TBTC
         </div>
         <div className="bond-duration">
           Bond duration: 6 months
@@ -43,7 +50,9 @@ const Congratulations = ({ depositAddress }) => {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    depositAddress: state.deposit.depositAddress
+    depositAddress: state.deposit.depositAddress,
+    lotInSatoshis: state.deposit.lotInSatoshis,
+    signerFeeInSatoshis: state.deposit.signerFeeInSatoshis,
   }
 }
 
