060c590dc43313325bf60769a96b5f769dbc9091
liamzebedee <liam.zebedee@keep.network>
2020-04-15 16:19:27 +1000

    Refactor chooseWallet code
    Colocate connector with other wallet info

--- src/components/lib/ConnectWalletDialog.js
+++ src/components/lib/ConnectWalletDialog.js
@@ -46,38 +46,27 @@ const SUPPORTED_CHAIN_IDS = [
 ]
 
 // Connectors.
-const injectedConnector = new InjectedConnector({
-	supportedChainIds: SUPPORTED_CHAIN_IDS
-})
+const injectedConnector = new InjectedConnector({})
 
 const ledgerConnector = new LedgerConnector({
-	// We use the chainId of mainnet here to workaround an issue with the ledgerjs library.
-	// It currently throws an error for the default chainId of 1377 used by Geth/Ganache.
-	// 
-	// The `v` value in ECDSA sigs is typically used as a recovery ID, but we also encode it 
-	// differently depending on the chain to prevent transaction replay (the so called chainId of EIP155).
-	// 
-	// At some point, Ledger had to update their firmware, to swap from a uint8 chainId to a uint32 chainId [1].
-	// 
-	// They updated their client library with a 'workaround' [2], but it doesn't appear to work.
-	// 
-	// [1]: https://github.com/LedgerHQ/ledger-app-eth/commit/8260268b0214810872dabd154b476f5bb859aac0
-	// [2]: https://github.com/LedgerHQ/ledgerjs/blob/master/packages/web3-subprovider/src/index.js#L143
-    chainId: 1,
+	// TODO: introduce CHAIN_ID and ETH_RPC_URL env variables.
+    chainId: process.env.CHAIN_ID || 1377,
 	url: 'ws://localhost:8545'
 })
+
 // Wallets.
 const WALLETS = [
 	{
 		name: "Metamask",
 		icon: "/images/metamask-fox.svg",
-		showName: true
+		showName: true,
+		connector: injectedConnector
 	},
 	{
 		name: "Ledger",
-		icon: "/images/ledger.svg"
+		icon: "/images/ledger.svg",
+		connector: ledgerConnector
 	},
-	}
 ]
 
 export const ConnectWalletDialog = ({ shown, onConnected, onClose }) => {
@@ -90,15 +79,8 @@ export const ConnectWalletDialog = ({ shown, onConnected, onClose }) => {
 		error
 	}
 
-	async function chooseWallet(wallet) {
+	async function chooseWallet(wallet, connector) {
 		setChosenWallet(wallet)
-
-		let connector
-		if (wallet == 'Ledger') {
-			connector = ledgerConnector
-		} else if (wallet == 'Metamask') {
-			connector = injectedConnector
-		}
 
 		try {
 			await activate(connector, undefined, true)
@@ -118,8 +100,8 @@ export const ConnectWalletDialog = ({ shown, onConnected, onClose }) => {
 
 			<ul className='wallets'>
 				{
-					WALLETS.map(({ name, icon, showName }) => {
-						return <li className='wallet-option' onClick={() => chooseWallet(name)}>
+					WALLETS.map(({ name, icon, showName, connector }) => {
+						return <li className='wallet-option' onClick={() => chooseWallet(name, connector)}>
 							<img src={icon} />
 							{showName && name}
 						</li>
