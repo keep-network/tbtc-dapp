66dd8b8ffba2af042a5b6f0dfec652f352ffaf01
ironng <ironng@users.noreply.github.com>
2020-07-14 18:54:23 -0700

    Use <a> element instead of window.open

--- src/components/redemption/Confirming.js
+++ src/components/redemption/Confirming.js
@@ -1,61 +1,48 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import StatusIndicator from '../svgs/StatusIndicator'
 
-class Confirming extends Component {
-  handleClickButton = () => {
-    const { txHash, btcNetwork } = this.props
+const Confirming = ({ txHash, btcNetwork, error }) => {
+  const blockExplorerUrl =
+    `https://blockstream.info/${btcNetwork === 'testnet' ? 'testnet/' : ''}tx/${txHash}`
 
-    window.open(
-      `https://blockstream.info/${btcNetwork === 'testnet' ? 'testnet/' : ''}tx/${txHash}`,
-      '_blank'
-    );
-  }
-
-  render() {
-    const {
-      error,
-      txHash
-    } = this.props
-
-    return (
-      <div className="confirming">
-        <div className="page-top">
-          <StatusIndicator pulse />
+  return (
+    <div className="confirming">
+      <div className="page-top">
+        <StatusIndicator pulse />
+      </div>
+      <div className="page-body">
+        <div className="step">
+          Step 4/6
         </div>
-        <div className="page-body">
-          <div className="step">
-            Step 4/6
-          </div>
-          <div className="title">
-            { error ? 'Error confirming transaction' : 'Confirming...' }
-          </div>
-          <hr />
-          <div className="description">
-            <p>We're waiting to confirm your transaction.</p>
-            {
-              txHash
-              ? <button
-                  onClick={this.handleClickButton}
-                  className="black"
-                  >
+        <div className="title">
+          { error ? 'Error confirming transaction' : 'Confirming...' }
+        </div>
+        <hr />
+        <div className="description">
+          <p>We're waiting to confirm your transaction.</p>
+          {
+            txHash
+            ? <button className="black">
+                <a href={blockExplorerUrl}
+                target="_blank" rel="noopener noreferrer">
                   Follow along in block explorer
-                </button>
-              : ''
-            }
-            {
-              error
-              ? <div className="error">
-                  { error }
-                </div>
-              : ''
-            }
-          </div>
+                </a>
+              </button>
+            : ''
+          }
+          {
+            error
+            ? <div className="error">
+                { error }
+              </div>
+            : ''
+          }
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
