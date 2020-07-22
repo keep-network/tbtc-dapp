9cdfab35612ed484c811b7b856a12bed783dc60c
ironng <ironng@users.noreply.github.com>
2020-07-22 16:26:07 -0700

    Remove unnecessary BN check in format util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,9 @@
 import { BitcoinHelpers } from '@keep-network/tbtc.js'
 
-import BN from "bn.js"
-
 import BigNumber from "bignumber.js"
 BigNumber.set({ DECIMAL_PLACES: 8 })
 
 export function formatSatsToBtc(sats) {
-    sats = BN.isBN(sats) ? sats.toString() : sats
-
-    return new BigNumber(sats)
+    return new BigNumber(sats.toString())
         .div(BitcoinHelpers.satoshisPerBtc.toString()).toString()
 }
