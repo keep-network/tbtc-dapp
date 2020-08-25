f25210cabe827e30a9610a10941d0a8963cc0d7b
Antonio Salazar Cardozo <antonio@keep.network>
2020-08-25 17:19:22 -0400

    When handling errors, deal with the possible reason property
    Web3 errors tend to encode a contract's revert reason as a reason
    property, independent of the message property (which doesn't mention the
    revert reason at all at the moment). logError now includes that reason
    in the console.log call, and uses it as the payload for the error action
    if it exists.

--- src/sagas/lib/index.js
+++ src/sagas/lib/index.js
@@ -1,14 +1,15 @@
 import { put } from "redux-saga/effects"
 
 export function* logError(errorActionType, error) {
-  const { message, stack } = error
+  const { message, reason, stack } = error
   yield put({
     type: errorActionType,
     payload: {
-      error: message,
+      error: reason ? `Error: ${reason}` : message,
     },
   })
   console.error({
+    reason,
     message,
     originalStack: stack.split("\n").map((s) => s.trim()),
     error,
