8d297b72e9a688fd6d311bc7c3063f8aa0270666
ironng <ironng@users.noreply.github.com>
2020-08-28 17:26:46 -0700

    Create a shared Confirming component

---
+++ src/components/lib/Confirming.js
@@ -0,0 +1,56 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Description from "./Description"
+import StatusIndicator from "../svgs/StatusIndicator"
+
+const Confirming = ({
+  className,
+  stepStatus,
+  error,
+  requiredConfirmations,
+  confirmations,
+  children,
+  extraMessage,
+}) => {
+  return (
+    <div className={className}>
+      <div className="page-top">
+        <StatusIndicator pulse />
+      </div>
+      <div className="page-body">
+        <div className="step">Step {stepStatus}</div>
+        <div className="title">
+          {error ? "Error confirming transaction" : "Confirming..."}
+        </div>
+        <hr />
+        <Description error={error}>
+          <div>
+            Waiting for {requiredConfirmations} transaction{" "}
+            {`confirmation${requiredConfirmations > 1 ? "s" : ""}`}.{" "}
+            {extraMessage}
+            <p>
+              {confirmations} / {requiredConfirmations} blocks confirmed
+            </p>
+          </div>
+          {children}
+        </Description>
+      </div>
+    </div>
+  )
+}
+
+Confirming.propTypes = {
+  className: PropTypes.string,
+  stepStatus: PropTypes.string,
+  error: PropTypes.string,
+  requiredConfirmations: PropTypes.number,
+  confirmations: PropTypes.number,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  extraMessage: PropTypes.string,
+}
+
+export default Confirming
