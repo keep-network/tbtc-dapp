5318f6c061b375b36ce1716d97c7d4368639fb7c
ironng <ironng@users.noreply.github.com>
2020-09-09 17:29:55 -0700

    Start implementing side nav layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { withRouter } from "react-router"
+import { Link } from "react-router-dom"
 import PropTypes from "prop-types"
 
 import { Footer, Header } from "./lib"
@@ -11,26 +12,38 @@ function App(props) {
     <div className="main">
       <div className="app">
         <Header />
+        <nav className="side-nav">
+          <ul>
+            <li>
+              <Link to="/">Overview</Link>
+            </li>
+            <li>
+              <Link to="deposit">Mint</Link>
+            </li>
+            <li>
+              <Link to="/redeem">Redeem</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="content">
+          <div className="warning">
+            <p>The safety of your funds is important to us.</p>
+            <p>
+              This dApp is in ALPHA and improper use may lead to LOSS OF FUNDS.
+            </p>
+            <p>
+              For more information and options please{" "}
+              <a href="https://discord.gg/Bpzfsgx">
+                visit our Discord community
+              </a>
+              .
+            </p>
+          </div>
 
-        <div className="warning">
-          <p>The safety of your funds is important to us.</p>
-          <p>
-            This dApp is in ALPHA and improper use may lead to LOSS OF FUNDS.
-          </p>
-          <p>
-            For more information and options please{" "}
-            <a href="https://discord.gg/Bpzfsgx">visit our Discord community</a>
-            .
-          </p>
+          {children}
         </div>
-
-        {children}
+        <Footer includeSubscription={location.pathname === "/"} />
       </div>
-      <Footer
-        includeSubscription={
-          location.pathname === "/" || location.pathname.startsWith("/news") // TODO: remove when proper CMS is selected
-        }
-      />
     </div>
   )
 }
